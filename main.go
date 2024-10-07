03fa85937a64e467aaa65d139cd4a3b97c3dc236
nishahirani25 <52832916+nishahirani25@users.noreply.github.com>
2024-10-07 22:27:02 +1100

    Update main.go

--- main.go
+++ main.go
@@ -1,71 +1,22 @@
-// package main
-//
-// import (
-// 	"fmt"
-//
-// 	"github.com/georgechieng-sc/interns-2022/folder"
-// 	"github.com/gofrs/uuid"
-// )
-//
-// func main() {
-// 	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
-//
-// 	res := folder.GetAllFolders()
-//
-// 	// example usage
-// 	folderDriver := folder.NewDriver(res)
-// 	orgFolder := folderDriver.GetFoldersByOrgID(orgID)
-//
-// 	folder.PrettyPrint(res)
-// 	fmt.Printf("\n Folders for orgID: %s", orgID)
-// 	folder.PrettyPrint(orgFolder)
-// }
-
-
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
 )
 
 func main() {
-	orgID := uuid.FromStringOrNil(folder.DefaultOrgID) // Assuming DefaultOrgID is a const
+	orgID := uuid.FromStringOrNil(folder.DefaultOrgID)
 
-	res := folder.GetAllFolders() // Fetch all folders
+	res := folder.GetAllFolders()
 
-	// Create a folder driver
+	// example usage
 	folderDriver := folder.NewDriver(res)
+	orgFolder := folderDriver.GetFoldersByOrgID(orgID)
 
-	// Get folders by orgID
-	orgFolders := folderDriver.GetFoldersByOrgID(orgID)
-
-	// Pretty print all folders
 	folder.PrettyPrint(res)
-
-	fmt.Printf("\nFolders for orgID: %s\n", orgID)
-	folder.PrettyPrint(orgFolders)
-
-	// Create a reader to take user input
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("Enter the folder name you want to query (or type 'exit' to quit): ")
-		folderName, _ := reader.ReadString('\n')
-		folderName = folderName[:len(folderName)-1] // Remove newline character
-
-		if folderName == "exit" {
-			break
-		}
-
-		// Get child folders for the provided folder name
-		childFolders := folderDriver.GetAllChildFolders(orgID, folderName)
-		fmt.Printf("\nChild Folders for '%s':\n", folderName)
-		folder.PrettyPrint(childFolders)
-	}
+	fmt.Printf("\n Folders for orgID: %s", orgID)
+	folder.PrettyPrint(orgFolder)
 }
-
-
