771a754d8ccc5770c6d8957944b27163d4ccec76
nishahirani25 <52832916+nishahirani25@users.noreply.github.com>
2024-10-07 22:27:48 +1100

    fix

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -25,26 +25,21 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 }
 
 func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
-	// Your code here...
 
 	folders := f.GetFoldersByOrgID(orgID)
 	children := []Folder{}
 	parentFound := false
 
-	// Iterate through the filtered folders
 	for _, folder := range folders {
-		// Check if the current folder is the parent
 		if folder.Name == name {
 			parentFound = true
 		}
 
-		// Check if the folder path starts with the parent folder name followed by a dot
 		if strings.HasPrefix(folder.Paths, name+".") {
 			children = append(children, folder)
 		}
 	}
 
-	// If the parent folder is not found, print an error message
 	if !parentFound {
 		fmt.Printf("Error: Folder '%s' does not exist in organization '%s'\n", name, orgID)
 	}
