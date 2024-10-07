07ca21f355990f71b43499504883f043a93c5ee5
nishahirani25 <52832916+nishahirani25@users.noreply.github.com>
2024-10-07 22:28:30 +1100

    fix2

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -5,10 +5,8 @@ import (
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
-	// "github.com/stretchr/testify/assert"
 )
 
-// feel free to change how the unit test is structured
 func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	t.Parallel()
 	orgID := uuid.Must(uuid.FromString("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7"))
@@ -37,7 +35,7 @@ func Test_folder_GetFoldersByOrgID(t *testing.T) {
         },
         {
             name:  "Case with no folders for different orgID",
-            orgID: uuid.Must(uuid.NewV4()), // Different orgID
+            orgID: uuid.Must(uuid.NewV4()), 
             folders: []folder.Folder{
                 {Name: "creative-scalphunter", OrgId: orgID, Paths: "creative-scalphunter"},
             },
@@ -46,8 +44,6 @@ func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
 
 		})
 	}
